d7834e26272ba2f94f73b22333e0f0c21fea36bd
VlastimilVasut <vasutvlastimil@gmail.com>
2023-11-22 16:52:32 +0100

    setting it up

--- plan-testingV/Login.xaml.cs
+++ plan-testingV/Login.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace plan_testingV
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interakční logika pro Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void open_signin(object sender, RoutedEventArgs e)
-        {
-            Signin signin = new Signin();
-            signin.Show();
-            this.Close();
         }
     }
 }
