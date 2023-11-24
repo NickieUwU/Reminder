dde234435d074a1063584ed271e4408bf834c7b9
VlastimilVasut <vasutvlastimil@gmail.com>
2023-11-24 19:52:56 +0100

    working on getting the date

--- plan-testingV/NewPlan.xaml.cs
+++ plan-testingV/NewPlan.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,19 +36,25 @@ namespace plan_testingV
         {
             string plan_name = txtPlanName.Text;
             string plan_desc = txtPlanDesc.Text;
-            string plan_date = txtPlanDate.Text;
+            int year = (int)comboYear.SelectedIndex;
+            int month = (int)comboMonth.SelectedIndex;
+            int day = (int)comboDay.SelectedIndex;
+            int hour = (int)comboHour.SelectedIndex;
+            int minute = (int)comboMinute.SelectedIndex;
             
 
-            if(string.IsNullOrEmpty(plan_name))
+
+            if (string.IsNullOrEmpty(plan_name))
             {
                 warning_label.Content = "Please enter the plan's name";
             }
-            if(plan_date == null)
+            if(comboYear.SelectedIndex == 0 || comboMonth.SelectedIndex == 0 || comboDay.SelectedIndex == 0 || comboHour.SelectedIndex == 0 || comboMinute.SelectedIndex == 0)
             {
-                 warning_label.Content = "Please enter the plan's date";
+                warning_label.Content = "Please enter the plan's date";
             }
-            if (!string.IsNullOrEmpty(plan_name) || !string.IsNullOrEmpty(plan_desc) || plan_date != null)
+            if (!string.IsNullOrEmpty(plan_name) || !string.IsNullOrEmpty(plan_desc) || comboYear.SelectedIndex!=0||comboMonth.SelectedIndex!=0||comboDay.SelectedIndex!=0||comboHour.SelectedIndex!=0||comboMinute.SelectedIndex!=0)
             {
+                DateTime plan_date = new DateTime(year, month, day, hour, minute, 0);
                 warning_label.Content = "";
                 string DBcon_string = "Data Source=DESKTOP-LKC2C9H\\TEW_SQLEXPRESS;Initial Catalog=Reminder;Integrated Security=True";
                 SqlConnection SQL_con = new SqlConnection(DBcon_string);
@@ -75,6 +82,64 @@ namespace plan_testingV
                 {
                     SQL_con.Close();
                 }
+            }
+        }
+
+        private void add_years(object sender, RoutedEventArgs e)
+        {
+            int date = DateTime.Now.Year;
+            for(int i=date; i < date+100; i++)
+            {
+                comboYear.Items.Add(i);
+            }
+        }
+
+        private void add_months(object sender, RoutedEventArgs e)
+        { 
+            for(int i=0; i < 12; i++)
+            {
+                comboMonth.Items.Add(i+1);
+            }
+        }
+
+        private void add_days(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                string ending = "";
+                if(i== 0)
+                {
+                    ending = "st";
+                }
+                else if(i == 1)
+                {
+                    ending = "nd";
+                }
+                else if(i == 2)
+                {
+                    ending = "rd";
+                }
+                else
+                {
+                    ending = "th";
+                }
+                comboDay.Items.Add($"{i+1}{ending}");
+            }
+        }
+
+        private void add_hours(object sender, RoutedEventArgs e)
+        {
+            for(int i=0; i < 24; i++)
+            {
+                comboHour.Items.Add(i);
+            }
+        }
+
+        private void add_minutes(object sender, RoutedEventArgs e)
+        {
+            for(int i=0; i < 60; i++)
+            {
+                comboMinute.Items.Add(i+1);
             }
         }
     }
